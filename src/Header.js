05ffaa5f46b0b38f4adcedf39642059dfe360bb2
shivamkumar011 <shivamsinha981@gmail.com>
2021-06-17 11:56:17 +0530

    img

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+ import image from './'
+
+function Header() {
+    return (
+        <div className="notification bar">
+        <img src={image} alt="">fag</img>
+            
+        </div>
+    )
+}
+
+export default Header
