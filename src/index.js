5fc924e1906422a7b0a4096b0133a09b369b6a62
Vivek Ankit <72314052+vian9@users.noreply.github.com>
2021-08-04 15:54:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { GlobalStyles } from './global-styles.js';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
